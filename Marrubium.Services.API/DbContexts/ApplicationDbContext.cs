56b9ecf95ccd72bc9a09d340a4ed9adedbc163c5
P4rpleSky <p4rplesky@mail.ru>
2023-04-30 10:58:39 +0300

    Add project files.

---
+++ Marrubium.Services.API/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Marrubium.Services.ProductAPI.DbContexts
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        { 
+
+        }
+
+
+    }
+}
