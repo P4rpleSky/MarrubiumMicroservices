2fad12b9a2341e658b85c8de8681ae77a5d6df10
P4rpleSky <p4rplesky@mail.ru>
2023-06-21 22:54:57 +0300

    AdminWeb project files added

---
+++ Marrubium.AdminWeb/Models/ProductRazorModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Marrubium.AdminWeb.Models;
+
+public class ProductRazorModel
+{
+    public class OrderRazorModel
+    {
+        public ProductDto Product { get; set; } = new ProductDto();
+        public SD.ViewType ViewType { get; set; }
+    }
+}
