630a401e7e65035d292bad0459b69b1a25a7cffc
P4rpleSky <p4rplesky@mail.ru>
2023-06-21 22:53:49 +0300

    Models now stored in their own project

---
+++ Marrubium.Services.ProductAPI.HttpModels/ProductCreateUpdateDto.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace Marrubium.Services.ProductAPI.HttpModels
+{
+    public class ProductCreateUpdateDto
+    {
+        public string Name { get; set; } = null!;
+
+        public int Price { get; set; }
+
+        public List<string> ProductTypes { get; set; } = null!;
+
+        public List<string> Functions { get; set; } = null!;
+
+        public List<string> SkinTypes { get; set; } = null!;
+
+        public string ImageUrl { get; set; } = null!;
+
+        public string Description { get; set; } = string.Empty;
+    }
+}
