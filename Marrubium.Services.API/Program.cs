fdd9b553e5862c067c34f6a2dd4f2fd9f9712c92
P4rpleSky <p4rplesky@mail.ru>
2023-07-02 14:52:32 +0300

    CORS policy fix for Web

--- Marrubium.Services.API/Program.cs
+++ Marrubium.Services.API/Program.cs
@@ -22,7 +22,6 @@ static IHostBuilder CreateHostBuilder(string[] args)
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddControllers();
@@ -34,7 +33,6 @@ builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
 builder.Services.AddGrpc();
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options =>
 {
@@ -43,6 +41,16 @@ builder.Services.AddSwaggerGen(options =>
         Title = "Marrubium.Services.ProductAPI",
         Version = "v1"
     });
+});
+
+const string apiPolicyName = "_myAllowSpecificOrigins";
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: apiPolicyName,
+        policyBuilder =>
+        {
+            policyBuilder.WithOrigins(builder.Configuration["WebUrl"]).AllowAnyHeader().AllowAnyMethod();
+        });
 });
 
 var app = builder.Build();
@@ -55,6 +63,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors(apiPolicyName);
 
 app.UseAuthorization();
 
